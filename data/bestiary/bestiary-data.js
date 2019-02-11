c34aa444e9244938f7817ebf95b7a980a41815a1
Anderson Contreira <anderson.contreira@gmail.com>
2019-02-11 21:53:54 -0200

    initial development of bestiary

--- data/bestiary/bestiary-data.js
+++ data/bestiary/bestiary-data.js
@@ -1,38 +1,448 @@
-[
-  {
-    "id": 70,
-    "tag": "minotaur",
-    "name": "Minotaur",
-    "class": "C",
-    "experience": "1000",
-    "level": 40,
-    "hp": 400,
-    "drop-items": [
-      "luxurius-sword",null
-    ],
-    "note": "Senhor dos labirintos - Impiedoso com seus servos, aqueles que erram com ele sofrem as consequências.",
-    "bio": "",
-    "properties": {
-      "DEF": 1,
-      "DMA": 1,
-      "STR": 0,
-      "INT": 1,
-      "LCK": 0
+var data = [
+	{
+	"id": 1,
+        "number": "001",
+        "tag": "skull-angel",
+        "name": "Skull Angel",
+        "class": "E",
+        "experience": "1",
+        "level": 1,
+        "hp": 30,
+		"subordinated": "Calypso",
+        "drop-items": [
+            null, null
+        ],
+        "note": "?",
+        "bio": "",
+        "properties": {
+            "DEF": 0,
+            "DMA": 0,
+            "STR": 0,
+            "INT": 0,
+            "LCK": 0
+        },
+        "attributes": {
+            "attack": [
+                
+            ],
+            "weakness": [
+                
+            ],
+            "tolerance": [
+                
+            ],
+            "immune": [
+                
+            ]
+        }	
+	},
+	{
+	"id": 2,
+        "number": "002",
+        "tag": "putrid-piranha",
+        "name": "Putrid piranha",
+        "class": "E",
+        "experience": "1",
+        "level": 1,
+        "hp": 30,
+		"subordinated": "Calypso",
+        "drop-items": [
+            null, null
+        ],
+        "note": "?",
+        "bio": "",
+        "properties": {
+            "DEF": 0,
+            "DMA": 0,
+            "STR": 0,
+            "INT": 0,
+            "LCK": 0
+        },
+        "attributes": {
+            "attack": [
+                
+            ],
+            "weakness": [
+                
+            ],
+            "tolerance": [
+                
+            ],
+            "immune": [
+                
+            ]
+        }	
+	},
+	{
+	"id": 3,
+        "number": "003",
+        "tag": "Gnome",
+        "name": "Gnome",
+        "class": "E",
+        "experience": "1",
+        "level": 1,
+        "hp": 45,
+		"subordinated": "Calypso",
+        "drop-items": [
+            null, null
+        ],
+        "note": "?",
+        "bio": "",
+        "properties": {
+            "DEF": 0,
+            "DMA": 0,
+            "STR": 0,
+            "INT": 0,
+            "LCK": 0
+        },
+        "attributes": {
+            "attack": [
+                
+            ],
+            "weakness": [
+                
+            ],
+            "tolerance": [
+                
+            ],
+            "immune": [
+                
+            ]
+        }	
+	},
+	{
+        "id": 6,
+        "number": "006",
+        "tag": "zombie",
+        "name": "Zombie",
+        "class": "E",
+        "experience": "10",
+        "level": 1,
+        "hp": 30,
+		"subordinated": "Calypso",
+        "drop-items": [
+            "luxurius-sword", null
+        ],
+        "note": "Lord of the mazes - Ruthless with his servants, those who err with him suffer the consequences.",
+        "bio": "",
+        "properties": {
+            "DEF": 1,
+            "DMA": 1,
+            "STR": 0,
+            "INT": 1,
+            "LCK": 0
+        },
+        "attributes": {
+            "attack": [
+                "physical", "thunder-lightning", "dark"
+            ],
+            "weakness": [
+                "water-ice", "wind"
+            ],
+            "tolerance": [
+                "physical", "fire", "earth-sand", "nature"
+            ],
+            "immune": [
+                "thunder-lightning"
+            ]
+        }
     },
-    "attributes": {
-      "attack": [
-        "physical","thunder-lightning","dark"
-      ],
-      "weakness": [
-        "water","wind"
-      ],
-      "tolerance": [
-        "physical","fire","stone","nature"
-      ],
-      "immune": [
-        "thunder-lightning"
-      ]
-    }
+    {
+	"id": 7,
+        "number": "007",
+        "tag": "thunder-skull-ghost",
+        "name": "Thunder Skull Ghost",
+        "class": "E",
+        "experience": "1",
+        "level": 1,
+        "hp": 40,
+		"subordinated": "Gladius",
+        "drop-items": [
+            null, null
+        ],
+        "note": "?",
+        "bio": "",
+        "properties": {
+            "DEF": 0,
+            "DMA": 0,
+            "STR": 0,
+            "INT": 0,
+            "LCK": 0
+        },
+        "attributes": {
+            "attack": [
+                
+            ],
+            "weakness": [
+                
+            ],
+            "tolerance": [
+                
+            ],
+            "immune": [
+                
+            ]
+        }	
+	},
+	{
+        "id": 70,
+        "number": "070",
+        "tag": "minotaur",
+        "name": "Minotaur",
+        "class": "B",
+        "experience": "1000",
+        "level": 70,
+        "hp": 700,
+		"subordinated": "Calypso",
+        "drop-items": [
+            "luxurius-sword", null
+        ],
+        "note": "Lord of the mazes - Ruthless with his servants, those who err with him suffer the consequences.",
+        "bio": "",
+        "properties": {
+            "DEF": 1,
+            "DMA": 1,
+            "STR": 0,
+            "INT": 1,
+            "LCK": 0
+        },
+        "attributes": {
+            "attack": [
+                "physical", "thunder-lightning", "dark"
+            ],
+            "weakness": [
+                "water-ice", "wind"
+            ],
+            "tolerance": [
+                "physical", "fire", "earth-sand", "nature"
+            ],
+            "immune": [
+                "thunder-lightning"
+            ]
+        }
 
-  },
+    },
+	{
+        "id": 145,
+        "number": "145",
+        "tag": "mythzor-soul",
+        "name": "Mythzor Soul",
+        "class": "B",
+        "experience": "1000",
+        "level": 70,
+        "hp": 700,
+		"subordinated": "Calypso",
+        "drop-items": [
+        ],
+        "note": "Lord of the mazes - Ruthless with his servants, those who err with him suffer the consequences.",
+        "bio": "",
+        "properties": {
+            "DEF": 1,
+            "DMA": 1,
+            "STR": 0,
+            "INT": 1,
+            "LCK": 0
+        },
+        "attributes": {
+            "attack": [
+                "physical", "thunder-lightning", "dark"
+            ],
+            "weakness": [
+                "water-ice", "wind"
+            ],
+            "tolerance": [
+                "physical", "fire", "earth-sand", "nature"
+            ],
+            "immune": [
+                "thunder-lightning"
+            ]
+        }
+
+    },
+	{
+        "id": 146,
+        "number": "146",
+        "tag": "?",
+        "name": "?",
+        "class": "B",
+        "experience": "1000",
+        "level": 70,
+        "hp": 700,
+		"subordinated": "Calypso",
+        "drop-items": [
+        ],
+        "note": "Lord of the mazes - Ruthless with his servants, those who err with him suffer the consequences.",
+        "bio": "",
+        "properties": {
+            "DEF": 1,
+            "DMA": 1,
+            "STR": 0,
+            "INT": 1,
+            "LCK": 0
+        },
+        "attributes": {
+            "attack": [
+                "physical", "thunder-lightning", "dark"
+            ],
+            "weakness": [
+                "water-ice", "wind"
+            ],
+            "tolerance": [
+                "physical", "fire", "earth-sand", "nature"
+            ],
+            "immune": [
+                "thunder-lightning"
+            ]
+        }
+
+    },
+	{
+        "id": 147,
+        "number": "147",
+        "tag": "cathacresys",
+        "name": "Cathacresys",
+        "class": "B",
+        "experience": "1000",
+        "level": 70,
+        "hp": 700,
+		"subordinated": "Calypso",
+        "drop-items": [
+        ],
+        "note": "Lord of the mazes - Ruthless with his servants, those who err with him suffer the consequences.",
+        "bio": "",
+        "properties": {
+            "DEF": 1,
+            "DMA": 1,
+            "STR": 0,
+            "INT": 1,
+            "LCK": 0
+        },
+        "attributes": {
+            "attack": [
+                "physical", "thunder-lightning", "dark"
+            ],
+            "weakness": [
+                "water-ice", "wind"
+            ],
+            "tolerance": [
+                "physical", "fire", "earth-sand", "nature"
+            ],
+            "immune": [
+                "thunder-lightning"
+            ]
+        }
+
+    },
+	{
+        "id": 148,
+        "number": "148",
+        "tag": "luxurius-human",
+        "name": "Luxurius (Human)",
+        "class": "B",
+        "experience": "1000",
+        "level": 70,
+        "hp": 700,
+		"subordinated": "Calypso",
+        "drop-items": [
+        ],
+        "note": "Lord of the mazes - Ruthless with his servants, those who err with him suffer the consequences.",
+        "bio": "",
+        "properties": {
+            "DEF": 1,
+            "DMA": 1,
+            "STR": 0,
+            "INT": 1,
+            "LCK": 0
+        },
+        "attributes": {
+            "attack": [
+                "physical", "thunder-lightning", "dark"
+            ],
+            "weakness": [
+                "water-ice", "wind"
+            ],
+            "tolerance": [
+                "physical", "fire", "earth-sand", "nature"
+            ],
+            "immune": [
+                "thunder-lightning"
+            ]
+        }
+
+    },
+	{
+        "id": 149,
+        "number": "149",
+        "tag": "luxurius-demon",
+        "name": "Luxurius (Demon)",
+        "class": "B",
+        "experience": "1000",
+        "level": 70,
+        "hp": 700,
+		"subordinated": "Calypso",
+        "drop-items": [
+        ],
+        "note": "Lord of the mazes - Ruthless with his servants, those who err with him suffer the consequences.",
+        "bio": "",
+        "properties": {
+            "DEF": 1,
+            "DMA": 1,
+            "STR": 0,
+            "INT": 1,
+            "LCK": 0
+        },
+        "attributes": {
+            "attack": [
+                "physical", "thunder-lightning", "dark"
+            ],
+            "weakness": [
+                "water-ice", "wind"
+            ],
+            "tolerance": [
+                "physical", "fire", "earth-sand", "nature"
+            ],
+            "immune": [
+                "thunder-lightning"
+            ]
+        }
+
+    },
+	{
+        "id": 150,
+        "number": "150",
+        "tag": "lucifer",
+        "name": "Lucifer",
+        "class": "S",
+        "experience": "1000",
+        "level": 99,
+        "hp": "????",
+		"subordinated": "Calypso",
+        "drop-items": [
+            "luxurius-sword", null
+        ],
+        "note": "Lord of the mazes - Ruthless with his servants, those who err with him suffer the consequences.",
+        "bio": "",
+        "properties": {
+            "DEF": 1,
+            "DMA": 1,
+            "STR": 0,
+            "INT": 1,
+            "LCK": 0
+        },
+        "attributes": {
+            "attack": [
+                "physical", "thunder-lightning", "dark"
+            ],
+            "weakness": [
+                "water-ice", "wind"
+            ],
+            "tolerance": [
+                "physical", "fire", "earth-sand", "nature"
+            ],
+            "immune": [
+                "thunder-lightning"
+            ]
+        }
+
+    },
 ]
